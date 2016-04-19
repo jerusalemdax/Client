335fb130afb492f8b13820360431b1cb12752cfd
jerusalemdax <jerusalemdax@gmail.com>
2016-04-19 21:13:21 +0800

    增加更新场景

---
+++ Source/Client/UpdateManager.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+/**
+ * 
+ */
+class CLIENT_API UpdateManager
+{
+public:
+	UpdateManager();
+	~UpdateManager();
+};
